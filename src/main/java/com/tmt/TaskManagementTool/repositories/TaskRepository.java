6b00e130aae3fa22b65a6c1f5abea799a382fb04
Neha Raj <neharaj710@gmail.com>
2023-05-28 06:06:26 -0700

    fixes

--- src/main/java/com/tmt/TaskManagementTool/repositories/TaskRepository.java
+++ src/main/java/com/tmt/TaskManagementTool/repositories/TaskRepository.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.tmt.TaskManagementTool.models.Task;
 
+@Repository
 public interface TaskRepository extends MongoRepository<Task, ObjectId>{
     List<Task> findTasksByCreatedBy(String username);
     List<Task> findTasksByStatus(String status);
